e7bc3d00f88a5ec7dcfed92af1fe674ceb25cb3d
ZarestiaDev <6247787+ZarestiaDev@users.noreply.github.com>
2023-03-22 00:39:21 +0100

    Fix radiogroup subwindows

--- campaign/scripts/item.lua
+++ campaign/scripts/item.lua
@@ -6,15 +6,15 @@ function typeChanged()
 	local sType = DB.getValue(getDatabaseNode(), "itemtype", "");
 	
 	if sType == "Abilities" then
-		main.setVisible(false);
+		content.setVisible(false);
 		abilities.setVisible(true);
 		history.setVisible(false);
 	elseif sType == "History" then
-		main.setVisible(false);
+		content.setVisible(false);
 		abilities.setVisible(false);
 		history.setVisible(true);
 	else
-		main.setVisible(true);
+		content.setVisible(true);
 		abilities.setVisible(false);
 		history.setVisible(false);
 	end
