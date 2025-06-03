29304f34368489973c583888248b3f0da7c8c6fb
ZarestiaDev <6247787+ZarestiaDev@users.noreply.github.com>
2025-06-03 18:24:21 +0200

    Old changes

--- campaign/scripts/item_main.lua
+++ campaign/scripts/item_main.lua
@@ -1,16 +1,16 @@
-function update()
-	if super and super.update then
-        super.update();
-    end
+-- function update()
+-- 	if super and super.update then
+--         super.update();
+--     end
 
-	local nodeRecord = getDatabaseNode();
-	local sSubType = DB.getValue(nodeRecord, "subtype"):lower();
-	local bLegacy = sSubType:match("legacy");
+-- 	local nodeRecord = getDatabaseNode();
+-- 	local sSubType = DB.getValue(nodeRecord, "subtype"):lower();
+-- 	local bLegacy = sSubType:match("legacy");
 
-	local bReadOnly = WindowManager.getReadOnlyState(nodeRecord);
-	local bID = LibraryData.getIDState("item", nodeRecord);
+-- 	local bReadOnly = WindowManager.getReadOnlyState(nodeRecord);
+-- 	local bID = LibraryData.getIDState("item", nodeRecord);
 
-	WindowManager.callSafeControlUpdate(self, "penalties", bReadOnly, not (bID and bLegacy));
-	parentcontrol.window.controlframe.setVisible(bLegacy);
-	parentcontrol.window.itemtype.setVisible(bLegacy);
-end
+-- 	WindowManager.callSafeControlUpdate(self, "penalties", bReadOnly, not (bID and bLegacy));
+-- 	parentcontrol.window.controlframe.setVisible(bLegacy);
+-- 	parentcontrol.window.itemtype.setVisible(bLegacy);
+-- end
